73ad839f7e6bd90f48e5f7a20100e21fedc2eebe
Plackett <johnnicholasdimitri@gmail.com>
2022-07-15 18:47:57 -0400

    House Tileset and Player Idle Animation

--- Assets/PMovement.cs
+++ Assets/PMovement.cs
@@ -5,24 +5,42 @@ using UnityEngine;
 public class PMovement : MonoBehaviour
 {
     public GameObject plr;
+    private int direction;
     public float speed;
+    private Animator animator;
     // Start is called before the first frame update
     void Start()
     {
-        
+        animator = this.GetComponent<Animator>();
+        direction = 0;
     }
 
     // Update is called once per frame
     void Update()
     {
         if(Input.GetAxis("Horizontal") > 0) {
+            animator.SetInteger("State", 1);
+            direction = 0;
             plr.transform.position = (plr.transform.position + new Vector3(speed, 0, 0));
         } else
         {
             if (Input.GetAxis("Horizontal") < 0)
             {
+                animator.SetInteger("State", 2);
+                direction = 1;
                 plr.transform.position = (plr.transform.position - new Vector3(speed, 0, 0));
+            } else
+            {
+                animator.SetInteger("State", 0);
             }
+        }
+        if(direction == 0)
+        {
+            this.gameObject.GetComponent<SpriteRenderer>().flipX = false;
+        }
+        if (direction == 1)
+        {
+            this.gameObject.GetComponent<SpriteRenderer>().flipX = true;
         }
 
     }
